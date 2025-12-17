f825ef94cfafff0e7e758e4399fa9dfdec013815
AquilaDavid <aquiladavid07@gmail.com>
2025-12-17 01:25:25 -0300

    feat: Adicição do modal de Usuarios
    Criando o objeto do Usuario

---
+++ models/Usuario.py
@@ -0,0 +1,35 @@
+class Usuario():
+    def __init__(self, id, nome, cpf, nascimento):
+        self.id = id
+        self.nome = nome
+        self.cpf = cpf
+        self.nascimento = nascimento
+
+    def __repr__(self):
+        return f'<Usuario {self.id} - {self.nome}>'
+
+    
+    def to_json(self):
+        return {
+            "id": self.id,
+            "nome": self.nome,
+            "cpf": self.cpf,
+            "nascimento": self.nascimento
+        }
+
+    
+    @staticmethod
+    def from_json(data: dict):
+        return Usuario(
+            data["id"],
+            data["nome"],
+            data["cpf"],
+            data["nascimento"]
+        )
+
+
+print(__name__)
+
+if __name__ == "__main__":
+    usuario = Usuario(1, "João", "00011122233", "2025-10-09")
+    
