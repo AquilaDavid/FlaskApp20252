220d8be67affcc27f91404253eba6d9b21fc7b83
AquilaDavid <aquiladavid07@gmail.com>
2025-12-23 19:16:49 -0300

    feat: mudança na chamada das funções

--- initdb.py
+++ initdb.py
@@ -1,6 +1,6 @@
 from helpers.database.sqlite_helper import criar_tabela, carregar_json
 
-if __name__ == "__main__":
-    criar_tabela()
-    carregar_json()
-    print("✅ Banco SQLite criado e populado com sucesso")
+criar_tabela()
+carregar_json()
+
+print("Banco criado e dados carregados com sucesso.")
